0c7c499e02b1d8ceef2ef3745416c5a4f3743586
qinghai <noteon@gmail.com>
2016-03-31 15:42:17 +0800

    inc to 0.3.11_1

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name:"qinghai:collection2",
-  version: "0.3.11",
+  version: "0.3.11_1",
   git: "https://github.com/noteon/meteor-collection2.git",
   summary: "Migrate from 0.8. Collection2"
 });
 
 Package.on_use(function(api) {
-  api.use('simple-schema@0.2.45_1', ['client', 'server']);
-  api.imply && api.imply('simple-schema', ['client', 'server']);
+  api.use('qinghai:simple-schema@0.2.45_1', ['client', 'server']);
+  api.imply && api.imply('qinghai:simple-schema', ['client', 'server']);
 
   // Allow us to detect 'insecure'.
-  api.use('insecure', {weak: true});
+  api.use('insecure@1.0.1', {weak: true});
 
   api.use(['underscore@1.0.1', 'deps@1.0.5', 'check@1.0.2', 'mongo-livedata@1.0.6'], ['client', 'server']);
   api.add_files(['collection2.js'], ['client', 'server']);
