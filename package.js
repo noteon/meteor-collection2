090ddaadf2f79d371ed631164d3849f3c91c6010
qinghai <noteon@gmail.com>
2016-03-31 15:34:29 +0800

    repack for pub

--- package.js
+++ package.js
@@ -1,20 +1,23 @@
 Package.describe({
-  summary: "Wraps Meteor.Collection to provide support for automatic validation of insert and update operations on the client and server, plus simple virtual field support."
+  name:"qinghai:collection2",
+  version: "0.3.11",
+  git: "https://github.com/noteon/meteor-collection2.git",
+  summary: "Migrate from 0.8. Collection2"
 });
 
 Package.on_use(function(api) {
-  api.use('simple-schema', ['client', 'server']);
+  api.use('simple-schema@0.2.45_1', ['client', 'server']);
   api.imply && api.imply('simple-schema', ['client', 'server']);
 
   // Allow us to detect 'insecure'.
   api.use('insecure', {weak: true});
 
-  api.use(['underscore', 'deps', 'check', 'mongo-livedata'], ['client', 'server']);
+  api.use(['underscore@1.0.1', 'deps@1.0.5', 'check@1.0.2', 'mongo-livedata@1.0.6'], ['client', 'server']);
   api.add_files(['collection2.js'], ['client', 'server']);
 });
 
 Package.on_test(function(api) {
-  api.use('collection2');
+  api.use('qinghai:collection2');
   api.use('test-helpers', 'server');
   api.use(['tinytest', 'underscore', 'ejson', 'ordered-dict',
     'random', 'deps']);
